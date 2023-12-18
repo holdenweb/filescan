007bc58a7e965f2ddfcb80a0af599d6c67ebbd16
Steve Holden <steve@holdenweb.com>
2023-12-18 08:40:08 +0000

    Blacken and move run summary to correct indentation level.

--- filescan.py
+++ filescan.py
@@ -7,15 +7,19 @@ from datetime import datetime
 
 from load_tokens import scan_tokens
 
-DEBUG = True # Think _hard_ before enabling DEBUG
+DEBUG = False  # Think _hard_ before enabling DEBUG
+
 
 def debug(*args, **kwargs):
     if DEBUG:
         print(*args, **kwargs)
 
+
 def scan_directory(base_dir, conn):
     started = datetime.now()
-    file_count = known_files = updated_files = unchanged_files = new_files = deleted_files = 0
+    file_count = (
+        known_files
+    ) = updated_files = unchanged_files = new_files = deleted_files = 0
     if not base_dir.startswith("/"):
         base_dir = os.path.abspath(base_dir)
     if not base_dir.endswith("/"):
@@ -24,7 +28,12 @@ def scan_directory(base_dir, conn):
     print(base_dir)
 
     for dir_path, dirnames, filenames in os.walk(base_dir):
-        for ignore_dir in ["__pycache__", "site-packages", ".git", ".ipynb_checkpoints"]:
+        for ignore_dir in [
+            "__pycache__",
+            "site-packages",
+            ".git",
+            ".ipynb_checkpoints",
+        ]:
             if ignore_dir in dirnames:
                 dirnames.remove(ignore_dir)
         if not dir_path.endswith("/"):
@@ -37,44 +46,66 @@ def scan_directory(base_dir, conn):
             try:
                 id, modified, hash, seen = conn.id_mod_hash_seen(dir_path, filename)
                 known_files += 1
-                if disk_modified != modified: # Changed since last scan
+                if disk_modified != modified:  # Changed since last scan
                     updated_files += 1
-                    hash = hashlib.sha256(open(current_file_path, "rb").read()).hexdigest()
+                    hash = hashlib.sha256(
+                        open(current_file_path, "rb").read()
+                    ).hexdigest()
                     conn.update_modified_hash_seen(id, disk_modified, hash)
                     scan_tokens(conn, current_file_path, hash)
                     debug("*UPDATED*", current_file_path)
                 else:
                     unchanged_files += 1
                     conn.update_seen(id)
-            except conn.DoesNotExist:   # New file
+            except conn.DoesNotExist:  # New file
                 new_files += 1
                 try:
-                    hash = hashlib.sha256(open(current_file_path, "rb").read()).hexdigest()
+                    hash = hashlib.sha256(
+                        open(current_file_path, "rb").read()
+                    ).hexdigest()
                     scan_tokens(conn, current_file_path, hash)
                 except FileNotFoundError:
                     hash = "UNHASHABLE"
                 conn.db_insert_location(filename, dir_path, disk_modified, hash)
                 debug("*CREATED*", current_file_path)
             conn.commit()
-        ct = conn.all_file_count()
-        deleted_files = conn.count_not_seen()
-        for dirname, filepath in conn.dir_files_not_seen():
-            debug("*DELETED*", os.path.join(dirname, filepath))
-        conn.delete_not_seen()
-        conn.commit()
+    ct = conn.all_file_count()
+    deleted_files = conn.count_not_seen()
+    for dirname, filepath in conn.dir_files_not_seen():
+        debug("*DELETED*", os.path.join(dirname, filepath))
+    conn.delete_not_seen()
+    conn.commit()
 
-    print(f"""\
+    print(
+        f"""\
 Known:      {known_files}
 Updated:    {updated_files}
 Unchanged:  {unchanged_files}
 New:        {new_files}
 Deleted:    {deleted_files}
 
-Total seen: {file_count}""")
+Total seen: {file_count}"""
+    )
 
-    conn.record_run(started, dir_path, file_count, known_files, updated_files, unchanged_files, new_files, deleted_files)
+    conn.record_run(
+        started,
+        dir_path,
+        file_count,
+        known_files,
+        updated_files,
+        unchanged_files,
+        new_files,
+        deleted_files,
+    )
 
-def main(args=sys.argv[1:], DEBUG=False, storage='postgresql', database='filescan', create=False):
+
+def main(
+    args=sys.argv[1:],
+    DEBUG=False,
+    storage="postgresql",
+    database="filescan",
+    create=False,
+):
 
     store_name = f"{storage}_store"
     store = importlib.import_module(store_name)
@@ -83,26 +114,29 @@ def main(args=sys.argv[1:], DEBUG=False, storage='postgresql', database='filesca
     for base_dir in args:
         scan_directory(base_dir, conn)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     if len(sys.argv) == 1:
-       sys.exit("Nothing to do!")
+        sys.exit("Nothing to do!")
 
     args = {}
     for name in "storage", "database", "create":
         args[name] = input(f"{name.capitalize()}: ")
     if not args["storage"]:
         args["storage"] = "postgresql"
-    args['create'] = (args['create'] == 'yes')
-    if args['create']:
-        answer = input(f"""
+    args["create"] = args["create"] == "yes"
+    if args["create"]:
+        answer = input(
+            f"""
 This operation will destroy any existing
 database with the following characteristics:
 
 Storage:    {storage}
 Database:   {database}
 
-Do you wish to proceed (yes/no): """)
+Do you wish to proceed (yes/no): """
+        )
         if answer != "yes":
             sys.exit("Aborted: user opted not to create a new database.")
     main(**args)
